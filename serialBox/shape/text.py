c659abcae5d02e0d12dd0601c81768754dceb033
BrookeH <Brooke@nas10-240-74-200.york.ac.uk>
2016-04-22 12:59:58 +0100

    numbers matching spec

--- serialBox/shape/text.py
+++ serialBox/shape/text.py
@@ -3,26 +3,42 @@ from serialBox import colors
 class text(abstractShape):
     def __init__(self, x, y, color, screen, value):
         abstractShape.__init__(self, x, y, color)
-        
-        a = [[1,0],[2,0]]
-        b = [[3,1],[3,2]]
-        c = [[3,3],[3,4]]
-        d = [[2,5],[1,5]]
-        e = [[0,3],[0,4]]
-        f = [[0,1],[0,2]]
-        g = [[1,4],[2,4]]
         self.screen = screen
         self.value = value
-        
-        self.numbers = {0:[a,b,c,d,e,f],
-                        1:[b,c],
-                        2:[a,b,g,e,d],
-                        3:[a,b,g,c,d]}
+        self.coordinates = []
+        self.numbers = {0:[[0,0],[1,0],[2,0],
+                           [0,1],      [2,1],
+                           [0,2],      [2,2],
+                           [0,3],      [2,3],
+                           [0,4],[1,4],[2,4]],
+
+                        1:[[2,0],
+                           [2,1],
+                           [2,2],
+                           [2,3],
+                           [2,4]],
+
+                        2:[[0,0],[1,0],[2,0],
+                                       [2,1],
+                           [0,2],[1,2],[2,2],
+                           [0,3],
+                           [0,4],[1,4],[2,4]],
+
+                        3:[[0,0],[1,0],[2,0],
+                                       [2,1],
+                           [0,2],[1,2],[2,2],
+                                       [2,3],
+                           [0,4],[1,4],[2,4]]
+
+                           }
     def draw(self):
         output = ""
-        for segment in self.numbers[self.value]:
-            for coordinates in segment:
-                output += self.formatPointToString(self.x+coordinates[0],self.y+coordinates[1],self.color)
+        self.coordinates = []
+        for coordinates in self.numbers[self.value]:
+          x = self.x+coordinates[0]
+          y = self.y+coordinates[1]
+          self.coordinates.append([x,y])
+          output += self.formatPointToString(x,y,self.color)
         self.screen.output(output)
 
     def update_output(self, newOutput):
@@ -32,9 +48,9 @@ class text(abstractShape):
 
     def clear(self):
         output = ""
-        for segment in self.numbers[self.value]:
-            for coordinates in segment:
-                output += self.formatPointToString(self.x+coordinates[0],self.y+coordinates[1],colors.DEFAULTBACKGROUND)
+        self.coordinates = []
+        for coordinates in self.numbers[self.value]:
+          output += self.formatPointToString(self.x+coordinates[0],self.y+coordinates[1],colors.DEFAULTBACKGROUND)
         self.screen.output(output)
 
 
